b72b1e35ebadee85a3d99f14d49aff6ba7e3f7d5
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-09 13:52:12 -0500

    Update engineerQuestions.js

--- src/engineerQuestions.js
+++ src/engineerQuestions.js
@@ -3,20 +3,53 @@ module.exports = [
     type: "input",
     name: "name",
     message: "What is the engineer's name?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please enter the engineer's name.");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     name: "id",
     message: "What is the engineer's employee ID?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please enter the engineer's employee ID.");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     name: "email",
     message: "What is the engineer's email address?",
+    validate: (input) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (emailRegex.test(input)) {
+        return true;
+      } else {
+        console.log("Please enter the engineer's email address.");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     name: "github",
     message: "What is the engineer's GitHub user name?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please enter the engineer's GitHub user name.");
+        return false;
+      }
+    },
   },
 ];
