09d83f6f264cfef4eee3a640a51d01cbf56d4d99
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-09 13:52:27 -0500

    Update generateIntern.js

--- src/generateIntern.js
+++ src/generateIntern.js
@@ -1,15 +1,15 @@
 function generateIntern(intern) {
   return `<article class="card">
-    <div class="card-header">
-      <h2>${intern.name}</h2>
-      <h3>Intern</h3>
-    </div>
-    <div class="card-body">
-      <p>ID: ${intern.id}</p>
-      <p>Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
-      <p>School: ${intern.school}</p>
-    </div>
-  </article>`;
+        <div class="card-header">
+          <h2>${intern.name}</h2>
+          <h3>Intern</h3>
+        </div>
+        <div class="card-body">
+          <p>ID: ${intern.id}</p>
+          <p>Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+          <p>School: ${intern.school}</p>
+        </div>
+      </article>`;
 }
 
 module.exports = generateIntern;
