4d3549be9aefac009cf4233a7823bd8ee4969802
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-09 13:52:31 -0500

    Update generateManager.js

--- src/generateManager.js
+++ src/generateManager.js
@@ -1,15 +1,15 @@
 function generateManager(manager) {
   return `<article class="card">
-      <div class="card-header">
-        <h2>${manager.name}</h2>
-        <h3>Manager</h3>
-      </div>
-      <div class="card-body">
-        <p>ID: ${manager.id}</p>
-        <p>Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-        <p>Office Number: ${manager.officeNumber}</p>
-      </div>
-    </article>`;
+        <div class="card-header">
+          <h2>${manager.name}</h2>
+          <h3>Manager</h3>
+        </div>
+        <div class="card-body">
+          <p>ID: ${manager.id}</p>
+          <p>Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+          <p>Office Number: ${manager.officeNumber}</p>
+        </div>
+      </article>`;
 }
 
 module.exports = generateManager;
