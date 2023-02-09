e303bfd3e763339f6b3e1f0d77fcb6ee0e48da4d
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-09 13:52:20 -0500

    Update managerQuestions.js

--- src/managerQuestions.js
+++ src/managerQuestions.js
@@ -3,20 +3,53 @@ module.exports = [
     type: "input",
     name: "name",
     message: "What is the team manager's name?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please enter the manager's name.");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     name: "id",
     message: "What is the team manager's employee ID?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please enter the manager's employee ID.");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     name: "email",
     message: "What is the team manager's email address?",
+    validate: (input) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (emailRegex.test(input)) {
+        return true;
+      } else {
+        console.log("Please enter the manager's email address.");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     name: "officeNumber",
     message: "What is the team manager's office number?",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        console.log("Please enter the manager's office number.");
+        return false;
+      }
+    },
   },
 ];
