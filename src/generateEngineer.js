2549d0afcc881683262b800778615208415f4db8
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-09 13:52:25 -0500

    Update generateEngineer.js

--- src/generateEngineer.js
+++ src/generateEngineer.js
@@ -1,16 +1,15 @@
 function generateEngineer(engineer) {
   return `<article class="card">
-      <div class="card-header">
-        <h2>${engineer.name}</h2>
-        <h3>Engineer</h3>
-      </div>
-      <div class="card-body">
-        <p>ID: ${engineer.id}</p>
-        <p>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-        <p>GitHub: ${engineer.github}</p>
-        <p>GitHub: <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</p>
-      </div>
-    </article>`;
+        <div class="card-header">
+          <h2>${engineer.name}</h2>
+          <h3>Engineer</h3>
+        </div>
+        <div class="card-body">
+          <p>ID: ${engineer.id}</p>
+          <p>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+          <p>GitHub: <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></p>
+        </div>
+      </article>`;
 }
 
 module.exports = generateEngineer;
